40b35da1ded57dab13906158594f91a215ba1274
jtruong2 <jtruong2@comcast.net>
2017-08-20 10:31:05 -0600

    boss mode complete :D

--- app/controllers/api/v1/merchants/customer_pending_invoices_controller.rb
+++ app/controllers/api/v1/merchants/customer_pending_invoices_controller.rb
@@ -2,10 +2,4 @@ class Api::V1::Merchants::CustomerPendingInvoicesController < ApplicationControl
   def show
     render json: Customer.customers_with_pending_invoices(params[:merchant_id])
   end
-
-  private
-
-  def safe_params
-    params.permit(:merchant_id)
-  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -12,6 +12,13 @@ class Customer < ApplicationRecord
   end
 
   def self.customers_with_pending_invoices(id)
-    find_by_sql ["SELECT * FROM customers JOIN (SELECT i.customer_id FROM invoices i JOIN transactions t ON i.id = t.invoice_id WHERE t.result = 'failed' AND i.merchant_id = ? EXCEPT SELECT i.customer_id FROM invoices i JOIN transactions t ON i.id = t.invoice_id WHERE t.result = 'success' AND i.merchant_id = ? ) only_failed ON only_failed.customer_id = customers.id", id, id]
+    find_by_sql ["SELECT * FROM customers JOIN
+      (SELECT i.customer_id FROM invoices i
+      JOIN transactions t ON i.id = t.invoice_id
+      WHERE t.result = 'failed' AND i.merchant_id = ?
+      EXCEPT SELECT i.customer_id FROM invoices i
+      JOIN transactions t ON i.id = t.invoice_id
+      WHERE t.result = 'success' AND i.merchant_id = ? )
+      only_failed ON only_failed.customer_id = customers.id", id, id]
   end
 end
