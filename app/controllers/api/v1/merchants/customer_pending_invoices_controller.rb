e4d185d85f725655e13c4a09283c6e73782b75f9
jtruong2 <jtruong2@comcast.net>
2017-08-20 10:21:30 -0600

    Boss level complete :D

--- app/controllers/api/v1/merchants/customer_pending_invoices_controller.rb
+++ app/controllers/api/v1/merchants/customer_pending_invoices_controller.rb
@@ -2,4 +2,10 @@ class Api::V1::Merchants::CustomerPendingInvoicesController < ApplicationControl
   def show
     render json: Customer.customers_with_pending_invoices(params[:merchant_id])
   end
+
+  private
+
+  def safe_params
+    params.permit(:merchant_id)
+  end
 end
